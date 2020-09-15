8450306ca44bfbd56aa56ead2656f4ea4dcb2702
tristangiguere <tristangiguere@gmail.com>
2020-09-14 23:35:40 -0400

    Make final changes

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+
+            switch(argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+
+                // Add additional exponential power function as per step 4:
+                case "exponential":
+                case "^":
+                    result = Math.Pow(argFirstNumber, argSecondNumber);
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized.");
+            }
+            return result;
+
+            // Add StringBuilder function as per step 2:
+            StringBuilder myResult = new StringBuilder(argFirstNumber+argOperation+argSecondNumber+"="+result);
+        }
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,37 +1,83 @@
-﻿using System;
+using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using calculatorEnging.Dll
 
 namespace SimpleCalculator
 {
     class Program
     {
+
         static void Main(string[] args)
+
         {
-            try
-            {
-                //Class to convert user input
-                InputConverter inputConverter = new InputConverter();
 
-                //Class to perform actual calculations
-                CalculatorEngine calculatorEngine = new CalculatorEngine();
+            Console.WriteLine("WELCOME TO THE ULTIMATE CALCULATOR! Do you want to calculate some numbers?");
 
-                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                string operation = Console.ReadLine();
+            Console.WriteLine("Enter YES or NO: ");
 
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+            // Create a string variable and get user input from the keyboard and store it in the variable
 
-                Console.WriteLine(result);
+            string userDecision = Console.ReadLine();
 
-            } catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+            if (userDecision = "yes") {
+
+
+                Console.WriteLine("Great! Welcome to the calculator!");
+
+                try
+                {
+                    //Class to convert user input
+                    InputConverter inputConverter = new InputConverter();
+
+                    //Class to perform actual calculations
+                    CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+
+                    double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                    double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                    string operation = Console.ReadLine();
+
+                    static double CheckNum(string num)
+                    try
+                    {
+                        double firstNumber = double.Parse(num);
+                        return firstNumber;
+                        double secondNumber = double.Parse(num);
+                        return secondNumber;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("You should enter a number, not a symbol, words or letters：");
+                        string operation = Console.ReadLine();
+                        return CheckNum(str);
+                    }
+                    Console.WriteLine("enter '+' or 'plus' to indicate your operation, such as: " + firstNumber + operation + secondNumber);
+                    double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                    Console.WriteLine(myResult);
+
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
             }
+
+
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Okay, see you next time!");
+            }
+
+
         }
     }
 }
